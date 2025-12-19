fa6a17d6e3edf4347a8908ab894f8d921da1f5ba
B Sai Aaditya <saiaadityacharyvsh@gmail.com>
2025-12-19 15:36:34 +0530

    Assignment1

---
+++ Assigment/Assigment1.py
@@ -0,0 +1,43 @@
+print('********************job application form***********************')
+# str
+name=str(input("enter the Name:" ))
+address=str(input("enter the address:"))
+mobile_number =str(input('enter your mobile number:'))
+Email_id=str(input('enter your Email id:'))
+branch=str(input('enter your Branch name: '))
+#int
+passing_year=int(input('enter the year of passing year:'))
+#str
+collage_name=str(input('enter the college name:'))
+#float
+graduation_cgpa=float(input('enter your cgpa:'))
+inter_percentage=float(input('enter your inter percentage:'))
+ssc_percentage=float(input('enter your ssc percentage:'))
+#set
+skills=set(input('enter your skills:').split(','))
+#str
+Experience=str(input('enter your experience if any else write no experience:'))
+projects=str(input('enter your project details:'))
+
+
+print("******************your details are as follows***********************")
+
+
+print('name :',name)
+
+print('address:',address)
+print('mobile number:',mobile_number)
+print('Email id:',Email_id)
+print('branch:',branch)
+print('passing year:',passing_year)
+print('collage name:',collage_name)
+print('graduation cgpa:',graduation_cgpa)
+print('inter percentage:',inter_percentage)
+print('ssc percentage:',ssc_percentage)
+print('skills:',skills)
+print('Experience:',Experience)
+print('projects:',projects)
+    
+
+
+
